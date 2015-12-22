ae8ac250191bdcb1eed9af5f82532f2c6c44efe0
heojihun <karais89@gmail.com>
2015-12-22 23:22:52 +0900

    데이터타입과 연산자.

---
+++ phpjs/2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />   
+    </head>
+    <body> 
+        <h1>javascript</h1>
+        <script>
+            document.write("10" + "10");
+        </script>
+        <h2>php</h2>
+        <?php
+        // php 에서 + 연산자는 숫자를 더한다는 기능을 함. php는 숫자로 변경해서 계산함.
+        // 문자와 문자를 더하고 싶으면 . 을 쓰면 됨.
+            echo "10"."10";
+        ?>       
+    </body>
+</html>
